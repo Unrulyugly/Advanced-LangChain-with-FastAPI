c66d1fcefcee9746a5acb23c72882bcb541ffc7f
Vinci_ <125575773+Unrulyugly@users.noreply.github.com>
2024-06-20 04:15:19 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,24 +1,41 @@
 import requests
-import dotenv
-from dotenv import find_dotenv, load_dotenv
-from langchain_openai import OpenAIEmbeddings
+import pandas as pd
+from langchain_nomic.embeddings import NomicEmbeddings
+from langchain_community.document_loaders import DataFrameLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import DirectoryLoader,TextLoader
-config = dotenv.dotenv_values(".env")
-load_dotenv(find_dotenv())
-OPENAI_API_KEY= config['OPENAI_API_KEY']
-embedding = OpenAIEmbeddings(openai_api_key= OPENAI_API_KEY)
 
-loader = DirectoryLoader('./FAQ', glob="**/*.txt", loader_cls=TextLoader)
+
+CONNECTION_STRING = "postgresql://langchain:langchain@localhost:6024/langchain"
+COLLECTION_NAME= "langchain_pg_embeddding"
+embeddings =NomicEmbeddings(model="nomic-embed-text-v1")
+file_path = 'google_engine/email_domain_results.csv'
+
+# Read the CSV file using pandas
+df = pd.read_csv(file_path, encoding='ISO-8859-1')
+
+# Load the data into documents using DataFrameLoader
+loader = DataFrameLoader(df, page_content_column='Snippet')
 docs = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1840,
+# Split text into chunks of 512 tokens, with 20% token overlap
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=612,
+    chunk_overlap=103,
 )
-
 documents = text_splitter.split_documents(docs)
+
+# Convert the documents into a list of dictionaries
 docs_data = [doc.dict() for doc in documents]
 
+# Prepare the data to be sent in the POST request
 url = "http://localhost:8000/index?cleanup=full"
 response = requests.post(url, json=docs_data)
-print(response.json())
+
+# Print the response text for debugging
+print(response.text)
+
+try:
+    # Print the JSON response from the server
+    print(response.json())
+except requests.exceptions.JSONDecodeError as e:
+    print(f"Error decoding JSON: {e}")
